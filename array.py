3e2b4802f64bdbde48466e9e94820fd35ce3c553
mike-PG65 <petergonye123@gmail.com>
2026-05-14 17:03:41 +0300

    arrays

---
+++ array.py
@@ -0,0 +1,17 @@
+students = ["Mike", "Janet", "Claud"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+
+#You can improve your code as follows
+cars = ["Subaru", "Volkswagen", "Mercedes"]
+
+for car in cars:
+    print(car)
+
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i + 1, students[i])
--- forloop.py
+++ forloop.py
@@ -1,6 +1,44 @@
 for i in (0, 1, 2):
     print("meow!!")
 
-
+#add range
 for i in range(3):
     print("meow!!")
+
+while True:
+    n = int(input("What's n "))
+
+    if n < 0:
+        continue
+    else:
+        break
+
+while True:
+    p = int(input("What's p "))
+
+    if p > 0:
+        break;
+
+for _ in range(p):
+    print("meow!!!!")
+
+
+#use functions for reusability
+
+def main():
+    meow(get_number());
+
+def get_number():
+
+    while True:
+        x = int(input("What's x "))
+
+        if x > 0:
+            return x;
+
+def meow(x):
+    for _ in range(x):
+        print("Moew!!")
+    
+
+main()
