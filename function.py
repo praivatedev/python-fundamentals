98cd0054696a2541dab1a4bd80df54fdde277057
mike-PG65 <petergonye123@gmail.com>
2026-05-14 10:04:04 +0300

    first commit

---
+++ function.py
@@ -0,0 +1,10 @@
+def hello(to="world"):
+    print("hello,", to)
+
+
+# Output using our own function
+name = input("What's your name? ")
+hello(name)
+
+# Output without passing the expected arguments
+hello()
---
+++ return.py
@@ -0,0 +1,8 @@
+def main() :
+    x = int(input("what is the value of x "))
+    print("x Squred is,", hello(x))
+
+def hello(n):
+    return n * n
+
+main()
